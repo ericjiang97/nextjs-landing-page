351b99c784c13d2136e61be415e8490204fef6b4
Eric Jiang <ericjiang97@gmail.com>
2021-02-21 21:57:28 +0800

    more links

--- components/LinkCard/index.tsx
+++ components/LinkCard/index.tsx
@@ -5,7 +5,7 @@ import LinkCardProps from "./types";
 const LinkCard: React.FC<LinkCardProps> = ({ link }) => {
   return (
     <a href={link.url} target={link.target || "_self"}>
-      <div className="max-w-md mx-auto  rounded-xl shadow-md overflow-hidden min-w-full md:max-w-2xl ring-2 border-grey-300 border-solid border-1">
+      <div className="max-w-md mx-auto my-3 rounded-xl shadow-md overflow-hidden min-w-full md:max-w-2xl ring-2 border-grey-300 border-solid border-1">
         <div className="md:flex">
           <div className="p-8 flex-1">
             <div className="text-center tracking-wide text-xl  text-indigo-500 font-semibold">
--- config/links.ts
+++ config/links.ts
@@ -3,6 +3,22 @@ const links: Link[] = [
     url: "https://ericjiang.dev",
     name: "Website",
   },
+  {
+    url: "https://photos.ericjiang.dev",
+    name: "Photos",
+  },
+  {
+    url: "https://linkedin.com/in/ericjiang97",
+    name: "LinkedIn",
+  },
+  {
+    url: "https://instagram.com/lorderikir",
+    name: "Instagram",
+  },
+  {
+    url: "https://instagram.com/ericj.cooks",
+    name: "Eric does cooking insta",
+  },
 ];
 
 export default links;
