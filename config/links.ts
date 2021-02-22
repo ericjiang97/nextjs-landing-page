8af3fe53b6ad39f1053079fbf4b9a934b32075ab
Eric Jiang <ericjiang97@gmail.com>
2021-02-22 19:58:05 +0800

    removing heroicons

--- config/links.ts
+++ config/links.ts
@@ -2,22 +2,10 @@ const links: Link[] = [
   {
     url: "https://ericjiang.dev",
     name: "Website",
-    icon: {
-      type: "fas",
-      name: "globe",
-    },
   },
   {
     url: "https://photos.ericjiang.dev",
     name: "Photos",
-  },
-  {
-    url: "https://linkedin.com/in/ericjiang97",
-    name: "LinkedIn",
-  },
-  {
-    url: "https://instagram.com/lorderikir",
-    name: "Instagram",
   },
   {
     url: "https://instagram.com/ericj.cooks",
